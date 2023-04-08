c84e4101ace60436cf7e6a1f8544bbe633a60ab9
Allen Yeh <allen_yeh@systemweb.com.tw>
2023-04-08 20:26:01 +0800

    ✨feature: pinia with storage API

---
+++ src/components/piniaCounter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Pinia</h2>
+    <p>Example: useCounterStore</p>
+    <p>counter.count: {{ counter.count }}</p>
+    <p>counter.doubleCount: {{ counter.doubleCount }}</p>
+    <p><button @click="counter.increment">Click Me</button></p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'PiniaCounter'
+}
+</script>
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+
+const counter = useCounterStore()
+</script>
+<style lang="scss" scoped>
+</style>
